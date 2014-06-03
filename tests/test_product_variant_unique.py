13f89bdac9ceedd616da304ad5112f063ef03661
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-06-03 11:36:52 +0200

    Increase version to 3.2 and cleanup test

--- tests/test_product_variant_unique.py
+++ tests/test_product_variant_unique.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 from decimal import Decimal
 import trytond.tests.test_tryton
@@ -118,6 +110,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             TestProductVariantCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
